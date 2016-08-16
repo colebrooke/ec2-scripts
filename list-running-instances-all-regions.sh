dbbaa00fb9c05f130717a2e5ec3f6b6ed4815d86
Justin Miller <justin.miller@cnc0023.local>
2016-08-16 16:41:41 +0100

    various improvements

--- list-running-instances-all-regions.sh
+++ list-running-instances-all-regions.sh
@@ -2,6 +2,14 @@
 
 # quick script to show all EC2 instances (at least, the name tags!) running in all AWS regions...
 
+type aws >/dev/null 2>&1 || { echo >&2 "The aws cli is required to rung this script."; exit 1; }
+
+AVAILABLE_PROFILES=$(cat ~/.aws/config | grep profile | cut -d' ' -f2 | tr -d '[]')
+echo "Avilable profiles:"
+echo ${AVAILABLE_PROFILES[*]}
+echo "Please enter the credentails profile you'd like to use:"
+read PROFILE
+
 REGIONS=$(aws ec2 describe-regions --region us-east-1 --output text | awk '{print $NF}')
 
 # list running machines in each region
@@ -9,7 +17,7 @@ for REGION in ${REGIONS[*]}; do
         if [ -n $REGION ]; then
                 echo "$REGION"
                 echo "=================="
-                aws ec2 describe-instances --query 'Reservations[*].Instances[*].Tags[?Key==`Name`].Value' --filters Name=instance-state-name,Values=running --output json --region $REGION --output text
+                aws ec2 describe-instances --profile $PROFILE --query 'Reservations[*].Instances[*].Tags[?Key==`Name`].Value' --filters Name=instance-state-name,Values=running --output json --region $REGION --output text
                 echo ""
         fi
 done
--- list-running-instances.sh
+++ list-running-instances.sh
@@ -2,19 +2,38 @@
 
 # quick script to show EC2 instances running in one AWS region (just the name tags!)
 
-# optional, set profile
-export AWS_DEFAULT_PROFILE=dr
+type aws >/dev/null 2>&1 || { echo >&2 "The aws cli is required to rung this script."; exit 1; }
 
+AVAILABLE_PROFILES=$(cat ~/.aws/config | grep profile | cut -d' ' -f2 | tr -d '[]')
+echo "Avilable profiles:"
+echo ${AVAILABLE_PROFILES[*]}
+echo "Please enter the credentails profile you'd like to use:"
+read PROFILE
+
+#export AWS_DEFAULT_PROFILE=dr
 
 REGIONS=$(aws ec2 describe-regions --region us-east-1 --output text | awk '{print $NF}')
-
 echo "Availble regions:"
 echo ${REGIONS[*]}
 
 echo ""
 echo "Please enter your selected region:"
 read SELECTED_REGION
-aws ec2 describe-instances --query 'Reservations[*].Instances[*].Tags[?Key==`Name`].Value' --filters Name=instance-state-name,Values=running --output json --region $SELECTED_REGION --output text
 
+echo ""
+
+aws ec2 describe-instances	--profile $PROFILE \
+				--query 'Reservations[].Instances[].[PrivateIpAddress,InstanceId,Tags[?Key==`Name`].Value[]]' \
+				--filters Name=instance-state-name,Values=running \
+				--output=text --region $SELECTED_REGION \
+				| sed 's/None$/None\n/' | sed '$!N;s/\n/ /'
+
+echo ""
+
+# for reference, this command lists name tags only
+#aws ec2 describe-instances --query 'Reservations[*].Instances[*].Tags[?Key==`Name`].Value' --filters Name=instance-state-name,Values=running --output json --region $SELECTED_REGION --output text
+
+# for reference, this command lists private network IPs only
+#aws ec2 describe-instances --query 'Reservations[*].Instances[*].NetworkInterfaces[*].PrivateIpAddresses[*].PrivateIpAddress' --output text  --region $SELECTED_REGION --filters Name=instance-state-name,Values=running 
 
 
